0dd3edc2fdf4ea0b8b517f9ec89b1e2e427d4f06
SimranSingh <48674523+ishvik@users.noreply.github.com>
2020-06-13 23:59:03 +0530

    Update OCR_Printed_Text.py
    OCR

--- freshlybuiltimagebol/OCR_Printed_Text.py
+++ freshlybuiltimagebol/OCR_Printed_Text.py
@@ -1,45 +1,41 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import pytesseract
+from cv2 import fastNlMeansDenoisingColored,cvtColor,bitwise_not,threshold,getRotationMatrix2D,warpAffine,filter2D,imread,THRESH_BINARY,COLOR_BGR2GRAY,THRESH_OTSU,INTER_CUBIC,BORDER_REPLICATE,minAreaRect
+from numpy import column_stack,array,where
+from matplotlib.pyplot import imshow,xticks,yticks
+from pytesseract import image_to_string,pytesseract 
 from PIL import Image
 
-class image_to_text:
-    def remove_noise(image):
-        image = cv2.fastNlMeansDenoisingColored(image,None,20,10,7,21)
-        return image
-    def remove_skew(image):
-        in_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        in_gray = cv2.bitwise_not(in_gray)
-        thresh_pic = cv2.threshold(in_gray, 0, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-        coords_x_y = np.column_stack(np.where(thresh_pic > 0))
-        angle = cv2.minAreaRect(coords_x_y)[-1]
-        if angle < -45:
-            angle = -(90 + angle)
-        else:
-            angle = -angle
-        (h, w) = image.shape[:2]
-        center_of_pic = (w // 2, h // 2)
-        M = cv2.getRotationMatrix2D(center_of_pic, angle, 1.0)
-        image = cv2.warpAffine(image, M, (w, h),flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-        return image
-    def shapness_blur(image):
-        sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-        image = cv2.filter2D(image, -1, sharpen_kernel)
-        return image
-    def to_text(image):
-        pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-        string_from_image = pytesseract.image_to_string(image,lang='eng')
-    ##plot image in output
-    def plot_image(image):
-        plt.imshow(image)
-        plt.xticks([])
-        plt.yticks([])
+def remove_noise(image):
+    image = fastNlMeansDenoisingColored(image,None,20,10,7,21)
+    return image
 
+def remove_skew(image):
+    in_gray = cvtColor(image, COLOR_BGR2GRAY)
+    in_gray = bitwise_not(in_gray)
+    thresh_pic = threshold(in_gray, 0, 255,THRESH_BINARY | THRESH_OTSU)[1]
+    coords_x_y = column_stack(where(thresh_pic > 0))
+    angle = minAreaRect(coords_x_y)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+    (h, w) = image.shape[:2]
+    center_of_pic = (w // 2, h // 2)
+    M = getRotationMatrix2D(center_of_pic, angle, 1.0)
+    image = warpAffine(image, M, (w, h),flags=INTER_CUBIC, borderMode=BORDER_REPLICATE)
+    return image
 
+def shapness_blur(image):
+    sharpen_kernel = array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+    image = filter2D(image, -1, sharpen_kernel)
+    return image
 
+def to_text(image):
+    pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    string_from_image = image_to_string(image,lang='eng')
+    return string_from_image
 
-
-
-
+##plot image in output
+def plot_image(image):
+    imshow(image)
+    xticks([])
+    yticks([])
