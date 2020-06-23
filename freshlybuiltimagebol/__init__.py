e9028a898aa609186dd01dceb122634ba51cd554
vishal sharma <vishalsh533@gmail.com>
2020-06-24 01:14:33 +0530

    added OCR_Printed_Text file

--- freshlybuiltimagebol/__init__.py
+++ freshlybuiltimagebol/__init__.py
@@ -1,7 +1,7 @@
-# Classimport
+# Class import
+
 from freshlybuiltimagebol.photo_se_nikali_awaj import PhotoAwaj
 from freshlybuiltimagebol.text_bol_uthega import ShabdDhwani
 from freshlybuiltimagebol.photo_se_text import PhotoShabd
 from freshlybuiltimagebol.natural_photo_se_text import NaturalPhotoShabd
-
-
+from freshlybuiltimagebol.OCR_Printed_Text import ImageProcess
