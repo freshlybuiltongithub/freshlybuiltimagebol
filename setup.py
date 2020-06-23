c007d9036db4a35b7b6e232eec4d3ed8eee7eb4f
vishal sharma <vishalsh533@gmail.com>
2020-06-24 00:53:59 +0530

    Added Description path for pip

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from distutils.core import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
 setup(
   name = 'freshlybuiltimagebol',         
   packages = ['freshlybuiltimagebol'],   
   version = '0.0.1b',     
   license='MIT',        
-  description = 'Image Bhi Bol Uthegi',   
+  description = 'Image Bhi Bol Uthegi',
+  long_description=README,
+  long_description_content_type="text/markdown",
   author = 'Vishal Sharma',                   
   author_email = 'vishalsharma.gbpecdelhi@gmail.com',      
   url = 'https://github.com/FreshlyBuilt/freshlybuiltimagebol',   
-  download_url = 'https://github.com/FreshlyBuilt/freshlybuiltimagebol/archive/v0.0.1b.tar.gz',  
+  download_url = 'https://github.com/FreshlyBuilt/freshlybuiltimagebol/archive/v0.0.2.tar.gz',  
   keywords = ['Image', 'Audio', 'Text'],   
   install_requires=[            
           'hyper',
