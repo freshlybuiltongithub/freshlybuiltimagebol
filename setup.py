06b4783dce9d23989a157ac49429b56b394bdc4e
vishal sharma <vishalsh533@gmail.com>
2020-06-24 13:02:57 +0530

    Updated structure

--- setup.py
+++ setup.py
@@ -1,24 +1,32 @@
 from distutils.core import setup
 from pathlib import Path
 
-# The directory containing this file
-HERE = Path(__file__).parent
+DESCRIPTION = """freshlybuiltimagebol is the library made to allow users to perform various
+                 operations with text and images including translation to different languages,
+                 generarting audio file from text,extracting text from image and many more."""
 
-# The text of the README file
-README = (HERE / "README.md").read_text()
+# The directory containing this file
+this_directory = path.abspath(path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+try:
+    with io.open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
 
 setup(
   name = 'freshlybuiltimagebol',         
   packages = ['freshlybuiltimagebol'],   
-  version = '0.0.2.2',     
+  version = '0.0.2.3',     
   license='MIT',        
   description = 'Photo Bhi Bol Uthega',
-  long_description=README,
+  long_description=long_description,
   long_description_content_type="text/markdown",
   author = 'Vishal Sharma',                   
   author_email = 'vishalsharma.gbpecdelhi@gmail.com',      
   url = 'https://github.com/FreshlyBuilt/freshlybuiltimagebol',   
-  download_url = 'https://github.com/FreshlyBuilt/freshlybuiltimagebol/archive/v0.0.2.2.tar.gz',  
+  download_url = 'https://github.com/FreshlyBuilt/freshlybuiltimagebol/archive/v0.0.2.3.tar.gz',  
   keywords = ['Image', 'Audio', 'Text'],   
   install_requires=[            
           'hyper',
@@ -41,9 +49,4 @@ setup(
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
   ],
-  entry_points={
-        "console_scripts": [
-            "freshlybuiltimagebol.__init__",
-        ]
-    },
 )
