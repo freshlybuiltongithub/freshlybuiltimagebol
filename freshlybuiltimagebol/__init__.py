03ceb92dc3f4d2a663a8434d99e723d893d87813
vishal sharma <vishalsh533@gmail.com>
2020-06-25 15:03:36 +0530

    updated download class

--- freshlybuiltimagebol/__init__.py
+++ freshlybuiltimagebol/__init__.py
@@ -5,4 +5,4 @@ from freshlybuiltimagebol.text_bol_uthega import ShabdDhwani
 from freshlybuiltimagebol.photo_se_text import PhotoShabd
 from freshlybuiltimagebol.natural_photo_se_text import NaturalPhotoShabd
 from freshlybuiltimagebol.OCR_Printed_Text import ImageProcess
-from freshlybuiltimagebol.downloader import Model_File as download
+from freshlybuiltimagebol.downloader import imagebol_model_downloader
