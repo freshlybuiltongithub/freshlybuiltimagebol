2b40e022a6765b89e26ec962a44bc969c115c669
Sandip Baidya <sandipbaidya17@gmail.com>
2020-11-19 20:26:43 +0530

    Removed all the spaces from doc strings

--- freshlybuiltimagebol/text_bol_uthega.py
+++ freshlybuiltimagebol/text_bol_uthega.py
@@ -5,9 +5,12 @@ from .bhasha_codes import bhasha_kosh
 
 
 class ShabdDhwani:
-    """A class used for translation and audio generation of texts.
+    """
+    
+    A class used for translation and audio generation of texts.
     
     The class :class:`ShabdDhwani` is made up of two two Hindi words: ``Shabd`` meaning words and ``Dhwani`` meaning sound. This class, therefore, lets you play with texts and their translations to different languages along with their sounds. You can translate a text and also listen to it, in a particular language.
+
     """
 
     bhasha_codes = dict(map(reversed, bhasha_kosh.items()))
@@ -17,7 +20,9 @@ class ShabdDhwani:
         return bhasha
 
     def shabd_se_dhwani(shabd, bhasha, filename):
-        """Listen to text in audio, in a specific language.
+        """
+        
+        Listen to text in audio, in a specific language.
         
         This function accepts a text and a corresponding language, and generates an audio file of that text in that language.
 
@@ -33,10 +38,7 @@ class ShabdDhwani:
         >>> ShabdDhwani.shabd_se_dhwani("This is a wonderful world.", "english", "trans.mp3")
         >>> os.startfile("trans.mp3")
 
-        .
-
         """
-
 
         bhasha = ShabdDhwani.code_se_naam(bhasha)
         anuvadak = Translator().translate(shabd, dest=bhasha)
@@ -44,7 +46,9 @@ class ShabdDhwani:
         anuvadit_file.save(filename)
 
     def shabd_ki_bhasha_badlo(shabd, bhasha):
-        """Translates a string from one language to other
+        """
+        
+        Translates a string from one language to other
         
         :param shabd: The text that you want to translate.
         :type shabd: string
@@ -57,15 +61,15 @@ class ShabdDhwani:
         >>> print(ShabdDhwani.shabd_ki_bhasha_badlo("I am so happy!", 'french'))
         Je suis très heureux.
 
-        .
-
         """
 
         bhasha = ShabdDhwani.code_se_naam(bhasha)
         return Translator().translate(shabd, dest=bhasha).text
 
     def bhasa_badlkr_kya_bole(shabd, bhasha):
-        """Translate a text from one language to another.
+        """
+        
+        Translate a text from one language to another.
         
         :param shabd: orginal text that needs to be translated
         :type shabd: string
@@ -78,13 +82,14 @@ class ShabdDhwani:
         >>>print(ShabdDhwani.bhasa_badlkr_kya_bole("I am so happy", "hindi"))
         yah ek adbhut duniya hai
 
-        .
         """
         bhasha = ShabdDhwani.code_se_naam(bhasha)
         return Translator().translate(shabd, dest=bhasha).pronunciation
 
     def shabd_ki_bhasha_jaano(shabd):
-        """Know the language of the text
+        """
+        
+        Know the language of the text
         
         :param shabd: text
         :type shabd: string
@@ -93,6 +98,7 @@ class ShabdDhwani:
 
         >>> print(ShabdDhwani.shabd_ki_bhasha_jaano("Itu oru aṟputamāṉa ulakam"))
         tamil
+
         """
         return bhasha_kosh[Translator().detect(shabd).lang]
 
